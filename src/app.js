f52b2a333afc3003e53c06edf53871e75775f329
villamizarA <99750370+villamizarA@users.noreply.github.com>
2023-08-30 20:58:20 -0300

    1 vez mas

--- src/app.js
+++ src/app.js
@@ -4,13 +4,12 @@ import cookieParser  from "cookie-parser";
 import cors from 'cors';
 import authRoutes from "./routes/auth.routes.js";
 import recordatoriosRoutes from "./routes/recordatorios.routes.js";
-import dotenv from 'dotenv';
 
-dotenv.config();
+
 
 const app = express()
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: 'http://localhost:5173',
     credentials:true,
 }));
 app.use(morgan("dev"));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import dotenv from 'dotenv';
 import app from "./app.js";
-import { connectDB } from "./db.js";
+import {connectDB} from "./db.js";
 
-dotenv.config();  // las variables de entorno d.env
 
-const port = process.env.PORT || 8080; 
-connectDB('backend');
 
-app.listen(port, () => {
-  console.log("Server running on port", port);
-});
+connectDB();
+app.listen(8080)
+console.log("server on port", 8080)
