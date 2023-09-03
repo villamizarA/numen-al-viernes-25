a581da691d726ae9537c97bc6089cb2073774a29
villamizarA <99750370+villamizarA@users.noreply.github.com>
2023-09-03 20:29:54 -0300

    Delete front-numen/src/schemas directory

--- front-numen/src/schemas/recordatorios.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator');
-
-// validación para la tarea
-export const taskSchema = [
-  body('title')
-    .notEmpty().withMessage('Title is required'),
-  body('description')
-    .notEmpty().withMessage('Description is required'),
-];
