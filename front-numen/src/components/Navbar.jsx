742e0eb4183430bb7f018fad97a3f659e860697c
andreaVillamizar <99750370+villamizarA@users.noreply.github.com>
2023-09-05 19:16:37 -0300

    Update Navbar.jsx

--- front-numen/src/components/Navbar.jsx
+++ front-numen/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useAuth } from "../context/authContext";
+import { useAuth } from "../context/AuthContext";
 import { ButtonLink } from "./utiles/ButtonLink";
 import { HiUserCircle } from "react-icons/hi";
 import { FaHome, FaBook, FaBars, FaTimes } from "react-icons/fa"; // Agregamos FaBars y FaTimes
